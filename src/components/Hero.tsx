ab7444ed0da258d31474c57f6360f9f1c8624c14
mo-genai <mogenai@outlook.com>
2026-05-29 08:58:54 +0300

    Use source hero video URL

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import { HeroDotGrid } from './HeroDotGrid'
 import { VariableProximity } from './VariableProximity'
 
 const HERO_VIDEO_SRC =
-  'https://res.cloudinary.com/dmp1fo2j4/video/upload/v1779897126/Hero_background_video_middle_w4q8kl.mp4'
+  'https://cdn.sanity.io/files/kzldmhbc/production/4b27394d27fa19f63c0577749b00572644637e6c.mp4'
 const HERO_POSTER_SRC =
   'https://res.cloudinary.com/dmp1fo2j4/image/upload/v1779896891/Hero_fallback_image_middle_zix3fb.png'
 
